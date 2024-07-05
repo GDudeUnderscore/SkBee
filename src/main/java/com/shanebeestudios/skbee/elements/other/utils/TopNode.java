5faed1c67aac6ea8eb63aedfc2a6f3fa4decf6bd
GDude_ <GQM3443@gmail.com>
2024-07-05 13:40:54 -0400

    f

---
+++ src/main/java/com/shanebeestudios/skbee/elements/other/utils/TopNode.java
@@ -0,0 +1,30 @@
+package com.shanebeestudios.skbee.elements.other.utils;
+
+public class TopNode {
+	Object index;
+	Object value;
+	int leaderboardLocation;
+
+	public TopNode(Object index, Object value, int leaderboardLocation) {
+		this.index = index;
+		this.value = value;
+		this.leaderboardLocation = leaderboardLocation;
+	}
+
+	public Object getIndex() {
+		return index;
+	}
+
+	public Object getValue() {
+		return value;
+	}
+
+	public int getLocation() {
+		return leaderboardLocation;
+	}
+
+	@Override
+	public String toString() {
+		return "#" + (getLocation() + 1) + " " + getIndex() + ": " + getValue();
+	}
+}
